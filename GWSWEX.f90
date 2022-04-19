43a78b5fc6efd41a6a5a4d08307525ee8fe31b77
veethahavya-CU-cz <veethahavya@natur.cuni.cz>
2022-04-19 17:44:43 +0200

    CHECKPOINT: vanG-Mualem kSM/GW correction and validation, sm/epv over 1 solved, and checks

--- GWSWEX.f90
+++ GWSWEX.f90
@@ -1,18 +1,34 @@
 module helpers
     implicit none
 contains
-    function kSM(s, ks)
-        real*8, intent(in) :: s, ks
-        real*8 :: kSM, n
-        n = 2.5
-        kSM = ks*s*(1-(1-(s)**1/(1-(1/n)))**(1-(1/n)))**2
+    function kSM(s, ks, vanG_pars)
+        real*8, intent(in) :: s, ks, vanG_pars(4)
+        real*8 :: kSM, theta_r, theta_s, n, m, sat
+        theta_r = vanG_pars(1)
+        theta_s = vanG_pars(2)
+        n = vanG_pars(4)
+        m = (1-(1/n))
+        if(s<0.1) then
+            sat = ((0.1-theta_r)/(theta_s-theta_r))
+        else
+            sat = ((s-theta_r)/(theta_s-theta_r))
+        end if
+        kSM = ks*sat*((1-(1-(sat)**(1/m))**m)**2)
     end function kSM
 
-    function kGW(s, ks)
-        real*8, intent(in) :: s, ks
-        real*8 :: kGW, n
-        n = 2.5
-        kGW = ks*s*(1-(1-(s)**1/(1-(1/n)))**(1-(1/n)))**2
+    function kGW(s, ks, vanG_pars)
+        real*8, intent(in) :: s, ks, vanG_pars(4)
+        real*8 :: kGW, theta_r, theta_s, n, m, sat
+        theta_r = vanG_pars(1)
+        theta_s = vanG_pars(2)
+        n = vanG_pars(4)
+        m = (1-(1/n))
+        if(s<0.1) then
+            sat = ((0.1-theta_r)/(theta_s-theta_r))
+        else
+            sat = ((s-theta_r)/(theta_s-theta_r))
+        end if
+        kGW = ks*sat*((1-(1-(sat)**(1/m))**m)**2)
     end function kGW
 end module helpers
 
@@ -22,11 +38,12 @@ module gwswex
     integer  :: elems, nts, dt
     logical, allocatable  :: chd(:)
     real*8, allocatable :: gok(:), bot(:), n(:), k(:), p(:,:), et(:,:)
+    real*8 :: vanG_pars(4)
 contains
 
-    subroutine build(el, ts, ts_size, gok_l, bot_l, n_l, k_l)
+    subroutine build(el, ts, ts_size, gok_l, bot_l, n_l, k_l, vanG_pars_l)
         integer, intent(in) :: el, ts, ts_size
-        real*8, intent(in) :: n_l, bot_l(:), gok_l(:), k_l(:)
+        real*8, intent(in) :: n_l, bot_l(:), gok_l(:), k_l(:), vanG_pars_l(4)
         elems = el
         nts = ts
         dt = ts_size
@@ -35,6 +52,7 @@ contains
         bot = bot_l
         n = n_l
         k = k_l
+        vanG_pars = vanG_pars_l
         open(unit=42, file="fort.log", status="replace")
         write(42,*) "built"
     end subroutine
@@ -59,7 +77,7 @@ contains
         real*8, intent(inout) :: gws(:,:), sws(:,:), sm(:,:), epv(:,:), gw_dis(:,:), sw_dis(:,:), sm_dis(:,:), &
             Qin(:,:), Qout(:,:), Qdiff(:,:)
         real*8 :: L, sw_et_deficit, excess_gw_vol, sm_eq, k_inf, inf, excess_p, inf_deficit, sw_inf, &
-            k_inf_gw, gw_inf, et_deficit, sw_et
+            k_inf_gw, inf_gw, et_deficit, sw_et
         integer :: e, t
         open(unit=42, file="fort.log", status="old")
         write(42,*) "run entered"
@@ -114,57 +132,57 @@ contains
                         write(42,*) "sm is", sm(e,t-1)
                         write(42,*) "epv is", epv(e,t-1)
                         write(42,*) "sm/epv", sm(e,t-1)/epv(e,t-1)
-                        k_inf = kSM(min(sm(e,t-1)/epv(e,t-1), 1.0)*n(e), k(e)) !calc K from wetness at the begining of this dt i.e. end of last dt
+                        k_inf = kSM(min(sm(e,t-1)/epv(e,t-1), 1.0)*n(e), k(e), vanG_pars) !calc K from wetness at the begining of this dt i.e. end of last dt
                         write(42,*) "got k", k_inf
-                        inf = max(min(k_inf*dt, p(e,t)*dt, epv(e,t-1)-sm(e,t-1)), 0.0)
-                        write(42,*) "inf is", inf
+                        inf = min(k_inf*dt, p(e,t)*dt)
+                        write(42,*) "inf aka p_sm is ", inf
                         excess_p = p(e,t)*dt - inf
-                        write(42,*) "excess_p", excess_p
+                        write(42,*) "excess p aka p_sw is ", excess_p
+                        write(42,*) "sws is", sws(e,t-1)
+                        write(42,*) "ET is", et(e,t)*dt
+                        sw_et = min(sws(e,t-1)+excess_p, et(e,t)*dt)
                         inf_deficit = k_inf*dt - inf
                         write(42,*) "inf_deficit", inf_deficit
-                        write(42,*) "sws is", sws(e,t-1)
-                        sw_inf = max(min(inf_deficit, sws(e,t-1), epv(e,t-1)-sm(e,t-1)), 0.0)
-                        sws(e,t) = sws(e,t-1) - sw_inf + excess_p
+                        sw_inf = min(inf_deficit, sws(e,t-1)+excess_p-sw_et)
                         write(42,*) "sw_inf", sw_inf
+                        sws(e,t) = sws(e,t-1) - sw_inf + excess_p - sw_et
+                        et_deficit = et(e,t)*dt - sw_et
+                        write(42,*) "sw et removed", sw_et
                         write(42,*) "sws calcd", sws(e,t)
-                        sm(e,t) = sm(e,t-1) + inf + sw_inf
+                        sm(e,t) = sm(e,t-1) + inf + sw_inf - et_deficit
+                        write(42,*) "sm et removed", et_deficit
                         write(42,*) "sm calcd", sm(e,t)
-                        sm_eq = vanGI(-L) !!!consider doing GW push and gw-sm bal after ET extraction
+                        sm_eq = vanGI(-L)
                         write(42,*) "gws is ", gws(e,t-1)
                         write(42,*) "vanGI called. sm_eq is ", sm_eq
-                        k_inf_gw = kGW(min(sm(e,t-1)/epv(e,t-1), 1.0)*n(e), k(e)) !calc K from current wetness (after P and SW inf)
-                        gw_inf = min(sm(e,t)-sm_eq, k_inf_gw*dt) !if sm<sm_eq, gw_inf is -ve ...
+                        k_inf_gw = kGW(min(sm(e,t)/epv(e,t-1), 1.0)*n(e), k(e), vanG_pars) !calc K from current wetness (after P and SW inf)
+                        inf_gw = min(sm(e,t)-sm_eq, k_inf_gw*dt) !if sm<sm_eq, inf_gw is -ve ...
                         write(42,*) "k_inf_gw is", k_inf_gw
-                        write(42,*) "gw_inf is", gw_inf
-                        sm(e,t) = sm(e,t) - gw_inf !... deficit sm gets added to sm from gw
+                        write(42,*) "inf_gw is", inf_gw
+                        sm(e,t) = sm(e,t) - inf_gw !... deficit sm gets added to sm from gw
                         write(42,*) "sm recalcd ", sm(e,t)
-                        gws(e,t) = gws(e,t-1) + gw_inf/n(e) !... and subtracted from gw
+                        gws(e,t) = gws(e,t-1) + inf_gw/n(e) !... and subtracted from gw
                         write(42,*) "gws calcd", gws(e,t)
                         if(gws(e,t)>gok(e)) then
                             excess_gw_vol = (gws(e,t)-gok(e))*n(e) + sm(e,t)
                             gws(e,t) = gok(e)
                             sm(e,t) = 0
                             sws(e,t) = sws(e,t) + excess_gw_vol
+                            write(42,*) "gws recalcd", gws(e,t)
                         end if
-                        write(42,*) "gws recalcd", gws(e,t)
-
-                        !ET removal and SM-GW rebalance
-                        write(42,*) "ET is", et(e,t)*dt
-                        sw_et = min(sws(e,t), et(e,t)*dt)
-                        sws(e,t) = sws(e,t) - sw_et
-                        et_deficit = et(e,t)*dt - sw_et
-                        write(42,*) "sw et removed", sw_et
-                        sm(e,t) = sm(e,t) - et_deficit
-                        write(42,*) "sm et removed", et_deficit
-                        write(42,*) "sm is", sm(e,t)
+                        epv(e,t) = (gok(e) - gws(e,t))*n(e)
+                        if(sm(e,t)>epv(e,t)) then
+                            sws(e,t) = sws(e,t) + (sm(e,t)-epv(e,t))
+                            sm(e,t) = epv(e,t)
+                        end if
                         sm_eq = vanGI(-(gok(e) - gws(e,t))) !!!gw-sm balancing: consider adding a convergence criteria here
                         write(42,*) "new sm_eq", sm_eq
-                        k_inf_gw = kGW(min(sm(e,t-1)/epv(e,t-1), 1.0)*n(e), k(e))*dt - max(gw_inf, 0.00) !subtract k_inf_gw already utilized and allow freely capilary rise beyond k_inf_gw
+                        k_inf_gw = kGW(min(sm(e,t)/epv(e,t), 1.0)*n(e), k(e), vanG_pars)*dt - max(inf_gw, 0.00) !subtract k_inf_gw already utilized and allow freely capilary rise beyond k_inf_gw
                         write(42,*) "k_inf_gw remaining", k_inf_gw
-                        gw_inf = min(sm(e,t)-sm_eq, k_inf_gw*dt)
-                        write(42,*) "addnl gw_inf", gw_inf
-                        sm(e,t) = sm(e,t) - gw_inf
-                        gws(e,t) = gws(e,t) + gw_inf/n(e)
+                        inf_gw = min(sm(e,t)-sm_eq, max(k_inf_gw*dt,0.0))
+                        write(42,*) "addnl inf_gw", inf_gw
+                        sm(e,t) = sm(e,t) - inf_gw
+                        gws(e,t) = gws(e,t) + inf_gw/n(e)
                         write(42,*) "sm-gw balanced", sm(e,t), gws(e,t)
 
                         epv(e,t) = (gok(e) - gws(e,t))*n(e)
--- GWSWEX_run.py
+++ GWSWEX_run.py
@@ -4,37 +4,44 @@ import numpy as np
 from scipy.integrate import quad
 import os
 import matplotlib.pyplot as plt
+#%%
+vanG_pars = np.array([0.1, 0.4, 0.42, 2.5], dtype=np.float64,order='F')
 
 def vanGI(d):
     def theta(h_c):
-        theta_s = 0.4
-        theta_r = 0.1
-        alpha = 0.4
-        n = 2.5
+        theta_r = vanG_pars[0]
+        theta_s = vanG_pars[1]
+        alpha = vanG_pars[2]
+        n = vanG_pars[3]
         m = (1-(1/n))
         return theta_r + ((theta_s - theta_r)/((1+(alpha*(abs(h_c)))**n))**m)
     return np.float64(quad(theta,d,0)[0])
 
 def kSM(s):
+    #vanG-Mualem (YT)
     ks = 3e-2
-    n = 2.5
+    theta_r = vanG_pars[0]
+    theta_s = vanG_pars[1]
+    n = vanG_pars[3]
     m = (1-(1/n))
-    k = ks*s*(1-(1-(s)**1/m)**m)**2
+    sat = ((s-theta_r)/(theta_s-theta_r))
+    k = ks*sat*((1-(1-(sat)**(1/m))**m)**2)
     return np.float64(k)
 
+#%%
 elems = int(1)
 nts = int(100+1)
 dt = int(600)
 gok = np.random.default_rng().uniform(-3, 3, elems) + 100
 bot = gok - 20
-n = np.full(elems, 0.3)
-k = np.full(elems, 3e-2)
-gwswex.build(elems, nts, dt, gok, bot, n, k)
+n = np.full(elems, 0.4)
+k = np.full(elems, 60e-3)
+gwswex.build(elems, nts, dt, gok, bot, n, k, vanG_pars)
 
 chd = np.full(elems, False, dtype=bool)
-p = np.full((elems,nts), 60e-4)
+p = np.full((elems,nts), 550e-5)
 p[:,-50:] = 45e-4
-et = np.full((elems,nts), 50e-4)
+et = np.full((elems,nts), 500e-5)
 gwswex.init(chd, p, et)
 
 gws, sws, sm, epv, gw_dis, sw_dis, sm_dis, Qin, Qout, Qdiff = np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F'), np.zeros((elems,nts),dtype=np.float64,order='F')
@@ -50,9 +57,9 @@ if not os.path.exists(fig_path):
     os.makedirs(fig_path)
 elem = 0
 plotWlev = True
-plotPrec = False
-plotDis = False
-plotBal = False
+plotPrec = True
+plotDis = True
+plotBal = True
 savefig = True
 dDPI = 90
 pDPI = 1600
