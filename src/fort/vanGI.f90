855446bc6fc8b2afa610a6c0378d0583b3d89e48
veethahavya-CU-cz <veethahavya@natur.cuni.cz>
2022-04-30 19:27:01 +0200

    init

--- src/fort/vanGI.f90
+++ src/fort/vanGI.f90
@@ -56,3 +56,24 @@ function vanGI_trap(d, trap_n)
             theta = theta_r + ((theta_s - theta_r)/((1+(alpha*(abs(h_c)))**n))**m)
         end function theta
 end function vanGI_trap
+
+
+function vanGI_fgsl(d)
+    USE fgsl
+    real*8 :: vanGI_trap
+    real*8, intent(inout) :: d
+    integer, intent(in) :: trap_n
+    d = -d/100
+    vanGI_trap = simpsons(theta, d, dble(0), trap_n)*100
+    contains
+        function theta(h_c)
+            real*8, intent(in) :: h_c
+            real*8 :: theta, theta_r, theta_s, alpha, n, m
+            theta_r = parms_vanGI(1)
+            theta_s = parms_vanGI(2)
+            alpha = parms_vanGI(3)
+            n = parms_vanGI(4)
+            m = (1-(1/n))
+            theta = theta_r + ((theta_s - theta_r)/((1+(alpha*(abs(h_c)))**n))**m)
+        end function theta
+end function vanGI_trap
